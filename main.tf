cb1503620efd4ad5f41c8323806d442895e1a41c
SampathiRajini <127491843+SampathiRajini@users.noreply.github.com>
2025-03-12 21:41:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-0927371b2080fb819"]
 tags = {
 Name = var.instance_names[count.index]
 }
